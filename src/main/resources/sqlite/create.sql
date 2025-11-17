3bacf4bb3efe1fc4e6694dc9ce3c8d86405e487b
Andrew Soozay <asousa2021@gmail.com>
2025-11-17 14:02:17 -0800

    Update create.sql

--- src/main/resources/sqlite/create.sql
+++ src/main/resources/sqlite/create.sql
@@ -16,3 +16,17 @@ CREATE TABLE Vaccines (
     Doses int,
     PRIMARY KEY (Name)
 );
+
+CREATE TABLE Patients (Username VARCHAR(255) PRIMARY KEY,
+                       Salt BINARY(16),
+                       Hash BINARY(16));
+
+CREATE TABLE Appointments(Appointment_ID INT PRIMARY KEY,
+                          Caregiver_Username VARCHAR(255),
+                          Patient_Username VARCHAR(255),
+                          Vaccine VARCHAR(255),
+                          Time date,
+                          FOREIGN KEY (Caregiver_Username) REFERENCES Availabilities.Username,
+                          FOREIGN KEY (Patient_Username) REFERENCES Patients.Username,
+                          FOREIGN KEY (Vaccine) REFERENCES Vaccines.Name,
+                          FOREIGN KEY (Time) REFERENCES Availabilities.Time;
